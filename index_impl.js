7c4731844dfde1449d1b1a53386f8f6eed779072
ste-xx <stefan.breitenstein@gmail.com>
2018-07-02 21:26:17 +0200

    [add test cases. add delete function. add partial create function]

--- index_impl.js
+++ index_impl.js
@@ -2,15 +2,66 @@ const snsApi = require('./snsApi.js');
 const response = require("cf-fetch-response");
 
 exports.handler = async (event, context) => {
-    switch (event.RequestType.toLowerCase()) {
+
+    let {
+        RequestType,
+        ResourceProperties: {
+            FilterPolicy,
+            SubscriptionArn,
+        }
+    } = event;
+
+    switch (RequestType.toLowerCase()) {
         case 'create': {
-      //todo      return create(event, context);
+            console.log(`Create sns filter policy`);
+            return create({FilterPolicy, SubscriptionArn})
+                .then(() => response.sendSuccess(event, context));
         }
         case 'update': {
+            console.log(`Update sns filter policy`);
+            if (!FilterPolicy || !SubscriptionArn) {
+                throw `missing mandatory argument: FilterPolicy=${FilterPolicy} SubscriptionArn=${SubscriptionArn}`
+            }
+            return;
             //todo:          return update(event, context);
         }
         case 'delete': {
-            //todo          return deleteFn(event, context);
+            console.log(`Delete sns filter policy`);
+            return deleteFn({SubscriptionArn})
+                .then(() => response.sendSuccess(event, context));
         }
     }
+};
+
+
+let create = async ({FilterPolicy, SubscriptionArn} = {}) => {
+    if (!FilterPolicy || !SubscriptionArn || !Array.isArray(FilterPolicy)) {
+        throw `missing mandatory argument: FilterPolicy=${FilterPolicy} SubscriptionArn=${SubscriptionArn}`
+    }
+
+    let value = FilterPolicy.reduce((acc, cur) => {
+        if (acc[cur.Attribute]) {
+            throw `Duplicate entry: ${cur.Attribute}`;
+        }
+        acc[cur.Attribute] = JSON.parse(cur.Policy);
+        return acc;
+    }, {});
+
+    return snsApi.setSubscriptionAttributes({
+        AttributeName: 'FilterPolicy',
+        SubscriptionArn: SubscriptionArn,
+        AttributeValue: JSON.stringify(value)
+    });
+};
+
+let deleteFn = async ({SubscriptionArn} = {}) => {
+    if (!SubscriptionArn) {
+        return;
+    }
+
+    return snsApi.setSubscriptionAttributes({
+        AttributeName: 'FilterPolicy',
+        SubscriptionArn: SubscriptionArn,
+        AttributeValue: '{}'
+    });
 };
--- test/test.js
+++ test/test.js
@@ -17,34 +17,27 @@ describe('sns-filter-policy', () => {
 
     let underTest;
 
-    let expectedValidSNSSetSubscriptionAttributes;
+    let cfCreateExactEvent;
 
-    let cfCreateEvent;
-    let cfUpdateEvent;
-    let cfDeleteEvent;
+    let cfContext = null;
 
     beforeEach(() => {
         successFake = fake.resolves("send suc");
         failFake = fake.resolves("send fail");
 
-        snsSetSubscriptionAttributesFake =  fake.resolves({status: 'successful'});
+        snsSetSubscriptionAttributesFake = fake.resolves({status: 'successful'});
 
         underTest = proxyquire('../index_impl.js', {
             'cf-fetch-response': {
                 sendSuccess: successFake,
                 sendFail: failFake
             },
-            '.snsApi/':{
-                setSubscriptionAttributes:snsSetSubscriptionAttributesFake
+            './snsApi': {
+                setSubscriptionAttributes: snsSetSubscriptionAttributesFake
             }
         });
 
-        expectedValidSNSSetSubscriptionAttributes = JSON.parse(fs.readFileSync('test/expectedValidSNSSetSubscriptionAttributes.json'))
-
-        cfCreateEvent = JSON.parse(fs.readFileSync('test/cfCreateEventData.json', 'utf8'));
-        cfUpdateEvent = JSON.parse(fs.readFileSync('test/cfUpdateEventData.json', 'utf8'));
-        cfDeleteEvent = JSON.parse(fs.readFileSync('test/cfDeleteEventData.json', 'utf8'));
-
+        cfCreateExactEvent = getEventData('cfCreateExactEventData.json');
     });
 
     afterEach(() => {
@@ -52,12 +45,145 @@ describe('sns-filter-policy', () => {
     });
 
     describe('Missing mandatory arguments should result into rejection', () => {
+        it('SubscriptionArn', async () => {
+            delete cfCreateExactEvent.ResourceProperties.SubscriptionArn;
+            return expect(underTest.handler(cfCreateExactEvent, cfContext)).be.rejected;
+        });
 
+        it('FilterPolicy', async () => {
+            delete cfCreateExactEvent.ResourceProperties.FilterPolicy;
+            return expect(underTest.handler(cfCreateExactEvent, cfContext)).be.rejected;
+        });
+    });
+
+    describe('Invalid event data should result into rejection', () => {
+        it('duplicate attribute', async () => {
+            return expect(underTest.handler(getEventData('cfCreateDuplicateAttributeEventData.json'), cfContext)).be.rejected;
+        });
     });
 
     describe('Failed service calls should result into rejection', () => {
+        it('failing sns.setSubscriptionAttributes call', async () => {
+            underTest = proxyquire('../index_impl.js', {
+                'cf-fetch-response': {
+                    sendSuccess: successFake,
+                    sendFail: failFake
+                },
+                './snsApi': {
+                    setSubscriptionAttributes: fake.rejects({reason: 'permission denied'})
+                }
+            });
+            return expect(underTest.handler(cfCreateExactEvent, cfContext)).be.rejected;
+        });
+    });
 
-    })
+    describe('Create new sns filter policy', () => {
+        it('with exact match', async () => {
+            await underTest.handler(cfCreateExactEvent, cfContext);
+            expectSuccessCFResponse();
 
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData: cfCreateExactEvent});
 
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedCreateExactSnsSetSubscriptionAttributes.json'));
+        });
+
+        it('with prefix', async () => {
+            let givenEventData = getEventData('cfCreatePrefixEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedCreatePrefixSnsSetSubscriptionAttributes.json'));
+        });
+
+        it('with anything but', async () => {
+            let givenEventData = getEventData('cfCreateAnythingButEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedCreateAnythingButSnsSetSubscriptionAttributes.json'));
+        });
+
+        it('with range', async () => {
+            let givenEventData = getEventData('cfCreateRangeEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedCreateRangeSnsSetSubscriptionAttributes.json'));
+        });
+    });
+
+    describe('Update sns filter policy', () => {
+
+        it('FilterPolicy', async () => {
+            let givenEventData = getEventData('cfUpdateFilterPolicyEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedUpdateFilterPolicySnsSetSubscriptionAttributes.json'));
+        });
+
+        it('SubscriptionArn', async () => {
+
+            let givenEventData = getEventData('cfUpdateSubscriptionArnEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedUpdateSubscriptionArnSnsSetSubscriptionAttributes.json'));
+            throw "delete old, create new";
+        });
+    });
+
+    describe('Delete sns filter policy', () => {
+        it('delete', async () => {
+            let givenEventData = getEventData('cfDeleteEventData.json');
+            await underTest.handler(givenEventData, cfContext);
+            expectSuccessCFResponse();
+
+            let [event, context] = successFake.firstCall.args;
+            expectByPass(event, context, {givenEventData});
+
+            let [snsSubAttribute] = snsSetSubscriptionAttributesFake.firstCall.args;
+            expect(snsSubAttribute).to.deep.equal(getExpectedResult('expectedDeleteSnsSetSubscriptionAttributes.json'));
+        });
+    });
+
+    function expectSuccessCFResponse() {
+        expect(successFake.callCount).to.equal(1);
+        expect(failFake.callCount).to.equal(0);
+    }
+
+    function expectByPass(event, context, {givenEventData} = {}) {
+        expect(event).to.deep.equal(givenEventData);
+        expect(context).to.deep.equal(cfContext);
+    }
+
+    function getEventData(file) {
+        return JSON.parse(fs.readFileSync(`test/${file}`, 'utf8'));
+    }
+
+    function getExpectedResult(file) {
+        return JSON.parse(fs.readFileSync(`test/${file}`, 'utf8'));
+    }
 });
